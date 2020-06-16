11e7a3e26b03b670ef045f329a3165cf1f8d5816
pyeonno <bogun1364@gmail.com>
2020-06-16 10:40:08 +0900

    add ttt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.argv.length == 2 ? process.env.token : "NzIyMDg3ODE2MTYyNTc0Mzg3.Xugh4A.rRzWKh5A_-Y4X_pfEUhXIKC3p78";
+const token = process.argv.length == 2 ? process.env.token : "NzIyMDg3ODE2MTYyNTc0Mzg3.XugilA.9zUPhgbupkCCkae_2JRLzOIVvXA";
 const welcomeChannelName = "안녕하세요";
 const byeChannelName = "안녕히가세요";
 const welcomeChannelComment = "어서오세요.";
