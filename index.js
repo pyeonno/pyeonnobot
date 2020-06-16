5b3cce2159a81e581635272b819f513ad54eefed
pyeonno <bogun1364@gmail.com>
2020-06-16 10:43:27 +0900

    add tokend

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.argv.length == 2 ? process.env.token : "NzIyMDg3ODE2MTYyNTc0Mzg3.XugilA.9zUPhgbupkCCkae_2JRLzOIVvXA";
+const token = process.argv.length == 2 ? process.env.token : "NzIyMDg3ODE2MTYyNTc0Mzg3.Xugjtg.9FWyYfqXI6MnF_PNkZ5I9VsABnA";
 const welcomeChannelName = "안녕하세요";
 const byeChannelName = "안녕히가세요";
 const welcomeChannelComment = "어서오세요.";
