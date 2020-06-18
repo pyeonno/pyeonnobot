f107ebf23be2c3abe560971fac8963a06053007a
pyeonno <bogun1364@gmail.com>
2020-06-18 09:44:01 +0900

    add reload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.argv.length == 2 ? process.env.token : "NzIyMDg3ODE2MTYyNTc0Mzg3.XuhOmA.phtxny_TJXveMHSQf7YraL380Ts";
+const token = process.argv.length == 2 ? process.env.token : "NzIyMDg3ODE2MTYyNTc0Mzg3.Xuq4vw.9s2b5Z71J5jqkYdjP1qwfhXBIjY";
 const welcomeChannelName = "안녕하세요";
 const byeChannelName = "안녕히가세요";
 const welcomeChannelComment = "어서오세요.";
